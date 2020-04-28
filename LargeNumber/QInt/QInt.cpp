a8b6e02c8372c468e6bae6abaf4de8b4d11266ce
Hà Minh Cường <55825733+haminhcuong2k@users.noreply.github.com>
2020-04-28 10:37:41 +0700

    i)
    & | ~ ^

--- LargeNumber/QInt/QInt.cpp
+++ LargeNumber/QInt/QInt.cpp
@@ -15,3 +15,42 @@ QInt::QInt(QInt& q)
 QInt::~QInt()
 {
 }
+
+QInt QInt::operator&(const QInt& qint)
+{
+	QInt result;
+	for (int i = 0; i < 4; i++)
+	{
+		result.data[i] = this->data[i] & qint.data[i];
+	}
+	return result;
+}
+
+QInt QInt::operator|(const QInt& qint)
+{
+	QInt result;
+	for (int i = 0; i < 4; i++)
+	{
+		result.data[i] = this->data[i] | qint.data[i];
+	}
+	return result;
+}
+
+QInt QInt::operator^(const QInt& qint)
+{
+	QInt result;
+	for (int i = 0; i < 4; i++)
+	{
+		result.data[i] = this->data[i] ^ qint.data[i];
+	}
+	return result;
+}
+
+QInt& QInt::operator~()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		this->data[i] = ~this->data[i];
+	}
+	return *this;
+}
--- LargeNumber/QInt/QInt.h
+++ LargeNumber/QInt/QInt.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <iostream>
 #include <string>
 
@@ -13,6 +13,13 @@ public:
 	QInt(QInt& q);
 	~QInt();
 
+	//Toán tử &.
+	QInt operator&(const QInt&);
+	//Toán tử |.
+	QInt operator|(const QInt&);
+	//Toán tử ^.
+	QInt operator^(const QInt&);
+	//Toán tử ~.
+	QInt& operator~();
 
-	
 };
